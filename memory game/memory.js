a2e76d4fea96a244f80d37a9e6edbd66b1850845
picard83 <picard83@hotmail.com>
2023-12-07 20:38:22 -0500

    memory game cards

--- memory game/memory.js
+++ memory game/memory.js
@@ -5,15 +5,34 @@ let card = document.querySelectorAll(".card");
 
 //getting each individual card/btn /////
 
-function individualCard() {
+function individualCardTransformation() {
   for (let i = 0; i < card.length; i++) {
     let solo = card[i];
     // solo.style.backgroundColor = "red";
     solo.addEventListener("click", function (e) {
       let targetCard = e.target;
-      console.log(targetCard);
-      targetCard.classList.toggle("card1");
+      if (targetCard.classList.contains("basketball")) {
+        targetCard.classList.toggle("hidden");
+        setTimeout(function () {
+          targetCard.classList.toggle("hidden");
+        }, 1000);
+      } else if (targetCard.classList.contains("football")) {
+        targetCard.classList.toggle("hidden2");
+        setTimeout(function () {
+          targetCard.classList.toggle("hidden2");
+        }, 1000);
+      } else if (targetCard.classList.contains("cricket")) {
+        targetCard.classList.toggle("hidden3");
+        setTimeout(function () {
+          targetCard.classList.toggle("hidden3");
+        }, 1000);
+      } else if (targetCard.classList.contains("tennis")) {
+        targetCard.classList.toggle("hidden4");
+        setTimeout(function () {
+          targetCard.classList.toggle("hidden4");
+        }, 1000);
+      }
     });
   }
 }
-individualCard();
+individualCardTransformation();
