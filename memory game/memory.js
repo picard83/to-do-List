aca5c03f9ecb54c59ea5095aa5e66bf7d2f15a86
picard83 <picard83@hotmail.com>
2023-12-11 17:31:55 -0500

    first card stayed fliped over

--- memory game/memory.js
+++ memory game/memory.js
@@ -2,37 +2,50 @@ const container = document.querySelector(".container");
 let topRow = document.querySelector(".toprow");
 let botttomRow = document.querySelector(".bottomrow");
 let card = document.querySelectorAll(".card");
+let card1 = document.getElementById("card1");
+let card4 = document.getElementById("card4");
+
+console.log(card1);
 
 //getting each individual card/btn /////
 
 function individualCardTransformation() {
   for (let i = 0; i < card.length; i++) {
     let solo = card[i];
-    // solo.style.backgroundColor = "red";
+
+    // 1 Make it so the first card stays flipped permanently until a second card is clicked
+    // 2 Check for matches
+    // 3 Flip/reset cards if they are not a mat
+    let firstClicked = false;
     solo.addEventListener("click", function (e) {
       let targetCard = e.target;
-      if (targetCard.classList.contains("basketball")) {
-        targetCard.classList.toggle("hidden");
-        setTimeout(function () {
-          targetCard.classList.toggle("hidden");
-        }, 1000);
-      } else if (targetCard.classList.contains("football")) {
-        targetCard.classList.toggle("hidden2");
-        setTimeout(function () {
-          targetCard.classList.toggle("hidden2");
-        }, 1000);
-      } else if (targetCard.classList.contains("cricket")) {
-        targetCard.classList.toggle("hidden3");
-        setTimeout(function () {
-          targetCard.classList.toggle("hidden3");
-        }, 1000);
-      } else if (targetCard.classList.contains("tennis")) {
-        targetCard.classList.toggle("hidden4");
-        setTimeout(function () {
-          targetCard.classList.toggle("hidden4");
-        }, 1000);
+
+      firstClicked = !firstClicked;
+
+      if (firstClicked) {
+        if (card1 === card4) {
+        } else {
+          // wait 1 second
+          // flip both cards back over
+          // Increment number of tries
+          setTimeout(function () {
+            targetCard.classList.toggle("card");
+          }, 1000);
+        }
       }
     });
+    // solo.addEventListener("click", function (e) {
+    //   let targetCard = e.target;
+    //   if (targetCard.classList.contains("basketball")) {
+    //     targetCard.classList.toggle("hidden");
+    //   } else if (targetCard.classList.contains("football")) {
+    //     targetCard.classList.toggle("hidden2");
+    //   } else if (targetCard.classList.contains("cricket")) {
+    //     targetCard.classList.toggle("hidden3");
+    //   } else if (targetCard.classList.contains("tennis")) {
+    //     targetCard.classList.toggle("hidden4");
+    //   }
+    // });
   }
 }
 individualCardTransformation();
